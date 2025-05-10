6ff14edd49f0f3875924d3781296edf8d8a55e2a
Lachlan <lachlanmcbride5@gmail.com>
2025-05-10 17:42:22 +1000

    added delete + CORS

--- go.mod
+++ go.mod
@@ -5,11 +5,11 @@ go 1.23.0
 toolchain go1.23.8
 
 require (
-	github.com/go-redis/redis v6.15.9+incompatible
 	github.com/gorilla/mux v1.8.1
+	github.com/redis/go-redis/v9 v9.8.0
 )
 
 require (
-	github.com/onsi/ginkgo v1.16.5 // indirect
-	github.com/onsi/gomega v1.37.0 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 )
--- main.go
+++ main.go
@@ -4,20 +4,26 @@ import (
 	"bufio"
 	"encoding/json"
 	"fmt"
+	"io"
 	"math/rand"
 	"net/http"
+	"net/url"
 	"os"
 
-	"github.com/go-redis/redis"
+	"context"
+
 	"github.com/gorilla/mux"
+	"github.com/redis/go-redis/v9"
 )
 
 var (
 	keyDB *redis.Client
+	ctx   = context.Background()
 )
 
 const (
 	fruit_key = "fruits"
+	gods_key  = "gods"
 )
 
 func ConnectDB() *redis.Client {
@@ -27,7 +33,7 @@ func ConnectDB() *redis.Client {
 		DB:       0,                // use default DB
 	})
 	//panic if no db connected
-	_, err := rdb.Ping().Result()
+	_, err := rdb.Ping(ctx).Result()
 	if err != nil {
 		panic(err)
 	}
@@ -48,7 +54,7 @@ func KDB_File2List(rdb *redis.Client, keyname string) {
 
 	for scanner.Scan() {
 		line := scanner.Text()
-		rdb.LPush(keyname, line)
+		rdb.LPush(ctx, keyname, line)
 
 		fmt.Println("added to '", keyname, "' ~", line)
 	}
@@ -56,17 +62,32 @@ func KDB_File2List(rdb *redis.Client, keyname string) {
 
 // get a random list item
 func KDB_rand_ListItem(rdb *redis.Client, keyname string) string {
-	listLen, err := rdb.LLen(keyname).Result() //
+	listLen, err := rdb.LLen(ctx, keyname).Result() //
 	if err != nil {
 		return err.Error()
 	}
 	random_i := rand.Intn(int(listLen)) // generate random index in range
-	result, err := rdb.Do("LRANGE", keyname, random_i, random_i).Result()
+	result, err := rdb.Do(ctx, "LRANGE", keyname, random_i, random_i).Result()
 	if err != nil {
 		return err.Error()
 	}
 	getItem := result.([]interface{})[0].(string)
 	return getItem
+}
+
+func KDB_add2list(rdb *redis.Client, keyname string, val string) string { // 1 is success
+	//result, err := rdb.Do("lpush", keyname, val).Result()
+
+	//test_db := ConnectDB()
+
+	//result, err := test_db.LPush(keyname, val).Result()
+	err := keyDB.LPush(ctx, keyname, val).Err()
+	if err != nil {
+		fmt.Println("something failed", err)
+		return "0"
+	}
+	fmt.Println(`"`, val, `"`, "-added to list-")
+	return "1"
 }
 
 func fruit_salad(rdb *redis.Client, n_fruits int) []string {
@@ -87,14 +108,124 @@ func helloResp(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("welcome 🐯"))
 }
 
+// get the fruits list
 func jsonFruits(w http.ResponseWriter, r *http.Request) {
-	input := getFruityJSON(keyDB, fruit_key)
+	input := KDB_list2JSON(keyDB, fruit_key)
 	w.Write(input)
 }
 
-func getFruityJSON(rdb *redis.Client, listKey string) []byte {
+type TestData struct {
+	Val string `json:"val"` // needs to be in caps to be exported
+}
+
+func KDB_del_str(key string, targetList string, kdb *redis.Client) (string, int) {
+	//kdb.LRem(ctx, targetList, 0, key)
+	result, err := kdb.Do(ctx, "LREM", targetList, 0, key).Result()
+	i_result := int(result.(int64))
+	fmt.Println("n removed:", i_result)
+	if err != nil {
+		fmt.Println("err:", err)
+		return key, 0
+	}
+	return key, i_result
+
+}
+
+func delGod(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json;charset=UTF-8")
+
+	defer r.Body.Close()
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Cannot read body", http.StatusBadRequest)
+		return
+	}
+	var data TestData
+
+	if body != nil {
+		//err := json.NewDecoder(body).Decode(&data)
+		err := json.Unmarshal(body, &data)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+	key, n_times := KDB_del_str(data.Val, "gods", keyDB)
+	outputStr := fmt.Sprintf(key, "N removed:", n_times)
+	w.Write([]byte(outputStr))
+}
+
+func postGod(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	//origin := req.Header.Get("Origin")
+	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Credentials", "true")
+	if r.Method == "POST" {
+		defer r.Body.Close()
+
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Cannot read body", http.StatusBadRequest)
+			return
+		}
+		var result string
+		var data TestData
+		fmt.Println("request body:", string(body))
+
+		if body != nil {
+			//err := json.NewDecoder(body).Decode(&data)
+			err := json.Unmarshal(body, &data)
+			if err != nil {
+				fmt.Println(err)
+			}
+			result = KDB_add2list(keyDB, "gods", data.Val)
+		}
+		outputStr := fmt.Sprintf("%s %s", data.Val, result)
+		fmt.Println(outputStr)
+		w.Write([]byte(result))
+	}
+}
+
+func testPost(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json;charset=UTF-8")
+	//params := mux.Vars(r)
+	pp, _ := url.ParseQuery(r.URL.RawQuery)
+
+	var key string
+	var val string
+	var data TestData
+
+	if r.Body != nil {
+		//fmt.Println("body:", r.Body)
+		ss := json.NewDecoder(r.Body).Decode(&data)
+		fmt.Println("Testdata:", data.Val, ss)
+	}
+
+	for k := range pp {
+		key = k
+		val = pp[k][0]
+	}
+	fmt.Println(key, val)
+	w.Write([]byte(key + ":" + val))
+}
+
+// get the gods list
+func jsonGods(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	//origin := req.Header.Get("Origin")
+	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET")
+	w.Header().Set("Access-Control-Allow-Credentials", "true")
+	input := KDB_list2JSON(keyDB, gods_key)
+	w.Write(input)
+}
+
+func KDB_list2JSON(rdb *redis.Client, listKey string) []byte {
 	var output []byte
-	values, err := rdb.LRange(listKey, 0, -1).Result()
+	values, err := rdb.LRange(ctx, listKey, 0, -1).Result()
 	if err != nil {
 		fmt.Printf("Failed to read Redis list: %v \n", err)
 		return output
@@ -109,17 +240,41 @@ func getFruityJSON(rdb *redis.Client, listKey string) []byte {
 	return jsonData
 }
 
+// simple post request --> /gods {"name":"Pazuzu"}
+// LPUSH gods "Pazuzu"
+// del
+
 func main() {
 	fmt.Println("hellos")
+
 	keyDB = ConnectDB()
-	fruit_salad(keyDB, 10)
+
+	// err := keyDB.LPush(ctx, "gods", "shebang").Err()
+	// fmt.Println(err)
+
+	//fruit_salad(keyDB, 10)
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", helloResp).Methods("GET")
 	r.HandleFunc("/jFruit", jsonFruits).Methods("GET")
+	r.HandleFunc("/jGods", jsonGods).Methods("GET")
+	r.HandleFunc("/new_god", postGod).Methods("POST", "OPTIONS")
+	r.HandleFunc("/del", delGod).Methods("DELETE", "OPTIONS")
 	srv := &http.Server{
 		Addr:    "localhost:8080",
 		Handler: r,
 	}
 	srv.ListenAndServe()
+
+	// 	36) "Zeus"
+	// 37) "Athena"
+	// 38) "Apollo"
+	// 39) "Artemis"
+	// 40) "Dionysus"
+	// 41) "Serapis"
+	// 42) "Hermes"
+	// 43) "Demeter"
+	// 44) "Aphrodite"
+	// 45) "Asclepius"
+
 }
